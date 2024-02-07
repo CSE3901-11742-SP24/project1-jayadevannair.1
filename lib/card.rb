d5702ae9bd5093ddfcf64ec0433af1fc5799afa1
Advait Jayadevan Nair <advaitjayadevannair@gmail.com>
2024-02-07 02:37:26 -0500

    Finished implementing core functionality and game play

--- lib/card.rb
+++ lib/card.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class Card
   # Constants for the valid options for each attribute
   NUMBERS = [1, 2, 3].freeze
-  SHAPES = [:diamond, :squiggle, :oval].freeze
-  SHADINGS = [:solid, :striped, :open].freeze
-  COLORS = [:red, :green, :purple].freeze
+  SHAPES = %i[diamond squiggle oval].freeze
+  SHADINGS = %i[solid striped open].freeze
+  COLORS = %i[red green purple].freeze
 
   # Reader methods for card attributes
   attr_reader :number, :shape, :shading, :color
@@ -28,10 +30,10 @@ class Card
 
   # Checks if three cards make a set
   def self.is_set?(card1, card2, card3)
-    NUMBERS.all? { |attr| all_same_or_all_different?(card1.number, card2.number, card3.number) } &&
-    SHAPES.all? { |attr| all_same_or_all_different?(card1.shape, card2.shape, card3.shape) } &&
-    SHADINGS.all? { |attr| all_same_or_all_different?(card1.shading, card2.shading, card3.shading) } &&
-    COLORS.all? { |attr| all_same_or_all_different?(card1.color, card2.color, card3.color) }
+    NUMBERS.all? { |_attr| all_same_or_all_different?(card1.number, card2.number, card3.number) } &&
+      SHAPES.all? { |_attr| all_same_or_all_different?(card1.shape, card2.shape, card3.shape) } &&
+      SHADINGS.all? { |_attr| all_same_or_all_different?(card1.shading, card2.shading, card3.shading) } &&
+      COLORS.all? { |_attr| all_same_or_all_different?(card1.color, card2.color, card3.color) }
   end
 
   # Helper method to check if all attributes are the same or all different
--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'card'
 require_relative 'grid'
 
@@ -20,33 +22,87 @@ class Game
         end
       end
     end
-    # deck.shuffle
-    deck
+    deck.shuffle
   end
 
   # Checks if there are any sets in the play area
   def any_sets?
-    @play_area.combination(3).any? { |combo| Card.is_set?(combo[0], combo[1], combo[2]) }
+    @play_area.combination(3).any? do |combo|
+      Card.is_set?(combo[0], combo[1], combo[2])
+    end
+  end
+
+  # Return set or nil
+  def hint_set
+    list = @play_area.each_index.to_a
+    list.combination(3).find { |combo| Card.is_set?(@play_area[combo[0]], @play_area[combo[1]], @play_area[combo[2]]) }
   end
 
   # Main game loop
   def play_game
-    puts "Welcome to the Game of Sets!"
-    Grid.print_grid(@deck)
-    # until @deck.empty? && !any_sets?
-    #   # Game logic goes here
-    #   # show the play area
-    #   # prompt the player to select a set
-    #   puts "Game in progress..."
-    #   # Placeholder for game logic
-    # end
+    puts 'Welcome to the Game of Sets!'
+    until @deck.empty? && !any_sets?
+      @play_area += @deck.shift(12 - @play_area.length) if @play_area.length < 12
 
-    puts "Game over!"
+      if @deck.empty? && !any_sets?
+        puts 'Game won!'
+        break
+      end
+
+      Grid.print_grid(@play_area)
+
+      puts "Cards in deck: #{@deck.length}"
+
+      puts 'Choose one of the following options:'
+      puts '1)3 more cards' if @deck.any?
+      puts '0)To enter a set'
+      puts '-1)Cheat :)'
+      input = gets.chomp.to_i
+      if input == 1
+        @play_area += @deck.shift(3)
+        next
+      elsif input == -1
+        set = hint_set
+        if set
+          Grid.print_grid(set.map { |index| @play_area[index] })
+          puts "Hint set: #{set.join(', ')}"
+          set.sort!
+          @play_area.delete_at(set[2])
+          @play_area.delete_at(set[1])
+          @play_area.delete_at(set[0])
+
+        else
+          puts 'No sets found. Adding 3 more cards'
+          @play_area += @deck.shift(3)
+          #   puts 'No set found, add more cards!'
+        end
+        next
+      end
+      puts 'Please enter the indexs of the cards that make a set:'
+      indexs = []
+
+      3.times do |i|
+        print "Index #{i + 1}: "
+        input = gets.chomp.to_i
+      end
+
+      puts "You entered: #{indexs.join(', ')}"
+
+      if !Card.is_set?(@play_area[indexs[0]], @play_area[indexs[0]], @play_area[indexs[0]])
+        puts 'The slected cards are not a set!'
+      else
+        puts 'The slected cards are a set!'
+        indexs.sort!
+        @play_area.delete_at(indexs[2])
+        @play_area.delete_at(indexs[1])
+        @play_area.delete_at(indexs[0])
+      end
+    end
+
+    puts 'Game over!'
     # Display final scores
   end
 end
 
 # Start the game
-if __FILE__ == $PROGRAM_NAME
-  Game.new
-end
+Game.new if __FILE__ == $PROGRAM_NAME
