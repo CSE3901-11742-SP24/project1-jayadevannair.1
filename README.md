15ac333d99aac86b28a46c821d2900fd67254523
Advait Jayadevan Nair <advaitjayadevannair@gmail.com>
2024-02-11 15:34:40 -0500

    Added comments, optimized code and finished readme

--- README.md
+++ README.md
@@ -1,9 +1,45 @@
 # The Game Of Sets
 
-## Dependencies
+Welcome to The Game of Sets, a challenging and engaging card game that tests your ability to identify patterns and sets among cards with various symbols. This game is implemented in Ruby, making it easy to run on any system with Ruby installed.
 
-* [Ruby](https://www.ruby-lang.org/en/).
+## Getting Started
 
-## Usage
+### Prerequisites
 
-Install deps: `gem install bundler && bundle install`.  Run `bundle exec rake run` to run the program.
+Before you can run The Game of Sets, you'll need to have Ruby installed on your computer. If you don't have Ruby installed, you can download and install it from the official Ruby website:
+
+- [Ruby Installation Guide](https://www.ruby-lang.org/en/documentation/installation/)
+
+This game has been tested with Ruby version 3.1 and above. Please ensure your Ruby version meets this requirement.
+
+### Installation
+
+1. **Clone the Repository**
+
+First, clone the repository to your local machine using Git:
+
+`git clone https://github.com/CSE3901-11742-SP24/project1-jayadevannair.1.git
+cd game-of-sets`
+
+2. **Install Dependencies**
+
+The Game of Sets uses Bundler to manage its dependencies. Install Bundler (if you haven't already) and then install the required gems:
+
+`gem install bundler
+bundle install`
+
+## Running the Game
+
+Once the installation is complete, you're ready to run the game:
+
+`bundle exec rake run`
+
+This command starts the game. Follow the on-screen instructions to play.
+
+## How to Play
+
+- The game will present you with a grid of cards, each with unique attributes such as shape, color, and number.
+- Your task is to identify a 'set' of three cards where each feature, when looked at individually, is either all the same on each card or all different.
+- The game offers options to add more cards to the play area, enter a set manually, or get a hint for a set.
+
+Enjoy the game, and may the best pattern recognizer win!
--- lib/grid.rb
+++ lib/grid.rb
@@ -1,24 +1,33 @@
 # frozen_string_literal: false
 
+# The Grid class represents a grid layout to display cards with various properties.
 class Grid
+  # Prints a grid layout of cards with visual attributes.
   def self.print_grid(cards)
-    c = cards.clone
-    i = 0
+    c = cards.clone # Creates a shallow copy of the cards array to avoid modifying the original array.
+    i = 0 # Counter for card numbering in the grid.
+
+    # Loops until all cards have been processed and printed.
     until c.empty?
-      line = c.shift([6, c.length].min)
+      line = c.shift([6, c.length].min) # Retrieves up to 6 cards from the array to form a line in the grid.
+      
+      # Initializes an array to hold textual representation of each card.
       cardsText = []
       line.each_with_index do |card, index|
-        cardsText[index] = card_as_text(card)
+        cardsText[index] = card_as_text(card) # Converts each card to its textual representation.
       end
 
+      # Prints each row of the card's textual representation.
       7.times do |row|
         cardsText.each_with_index do |card, index|
+          # Adds color to the card text and prints it with spacing for alignment.
           print "#{add_color(card[(row * 10)..9 + (row * 10)], line[index])}    "
         end
         $stdout.flush
         puts ''
       end
 
+      # Prints card indices below each card for identification.
       line.length.times do
         print "    #{'%2d' % i}        "
         i += 1
@@ -30,87 +39,59 @@ class Grid
     nil
   end
 
+  # Generates a textual representation of a card based on its attributes.
   def self.card_as_text(card)
-    shape = card_shape(card)
-    text = card_top_bottom(card)
-    text += card_side(card)
-    l1 = card_side(card)
-    l2 = card_side(card)
-    l3 = card_side(card)
+    shape = card_shape(card) # Determines the shape symbol for the card.
+    text = card_top_bottom(card) + card_side(card) # Generates the top and side border based on shading.
+
+    # Generates the sides and content of the card based on shading and number.
+    l1, l2, l3 = Array.new(3) { card_side(card) }
     l2[4..5] = shape if card.number == 1 || card.number == 3
     if card.number == 2 || card.number == 3
-      l1[2..3] = shape
-      l3[6..7] = shape
+      l1[2..3], l3[6..7] = shape, shape
     end
-    text += l1 + l2 + l3
-    text += card_side(card)
-    text += card_top_bottom(card)
+
+    # Constructs the complete card text by concatenating all parts.
+    text += l1 + l2 + l3 + card_side(card) + card_top_bottom(card)
     text
   end
 
+  # Generates the top or bottom border of a card based on its shading.
   def self.card_top_bottom(card)
-    if card.shading == :solid
-      '██████████'
-    elsif card.shading == :striped
-      '##########'
-    else
-      '          '
+    case card.shading
+    when :solid then '██████████'
+    when :striped then '##########'
+    else '          '
     end
   end
 
+  # Generates the side borders of a card based on its shading.
   def self.card_side(card)
-    if card.shading == :solid
-      '█        █'
-    elsif card.shading == :striped
-      '#        #'
-    else
-      '          '
+    case card.shading
+    when :solid then '█        █'
+    when :striped then '#        #'
+    else '          '
     end
   end
 
+  # Determines the shape symbol for a card based on its shape attribute.
   def self.card_shape(card)
-    if card.shape == :diamond
-      '<>'
-    elsif card.shape == :squiggle
-      '~~'
-    else
-      '()'
+    case card.shape
+    when :diamond then '<>'
+    when :squiggle then '~~'
+    else '()'
     end
   end
 
+  # Adds color to a given text based on the card's color attribute.
   def self.add_color(text, card)
-    if card.color == :red
-      text.red
-    elsif card.color == :green
-      text.green
-    else
-      text.magenta
-    end
-  end
-end
-
-class String
-  def red
-    "\e[31m#{self}\e[0m"
-  end
-
-  def green
-    "\e[32m#{self}\e[0m"
-  end
-
-  def magenta
-    "\e[35m#{self}\e[0m"
-  end
-
-  def bold
-    "\e[1m#{self}\e[22m"
-  end
-
-  def italic
-    "\e[3m#{self}\e[23m"
-  end
-
-  def underline
-    "\e[4m#{self}\e[24m"
+    # Determines the ANSI color code based on the card's color attribute.
+    color_code = case card.color
+                 when :red then "\e[31m" # Red
+                 when :green then "\e[32m" # Green
+                 else "\e[35m" # Magenta
+                 end
+    # Wraps the text in the appropriate ANSI color codes, resetting the color at the end.
+    "#{color_code}#{text}\e[0m"
   end
 end
